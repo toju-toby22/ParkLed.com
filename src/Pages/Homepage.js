98f5093918a01a46bbf5e6ae522cb785b03e8c42
Tobi Otoju <ttotoju@gmail.com>
2022-11-29 17:39:03 +0100

    deleted items-center

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -37,10 +37,10 @@ function Homepage() {
 
             <div className='w-full h-screen crn bg-zinc-100 flex flex-col justify-between'>
                 <div className='grid items-center  md:grid-cols-2 max-w-[1240px] p-auto m-auto'>
-                    <div className='flex flex-col items-center md:items-start w-full h-screen px-3 py-12'>
-                        <p className='text-1xl'>Unique training on E-commerce</p>
+                    <div className='flex flex-col  md:items-start w-full h-screen px-3 py-12'>
+                        <p className='text-xl'>Unique training on E-commerce</p>
                         <h1 className='py-3 text-5xl md:text-7xl font-bold'>Digital Marketing</h1>
-                        <p className='text-2lg'>The Future of E-commerce</p>
+                        <p className='text-xl'>The Future of E-commerce</p>
                         <Link to="/courses"><button className='py-4 px-8 sm:w[60%] my-5'>Apply Here</button></Link>
                     </div>
                     <div>
